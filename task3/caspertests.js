ac19c77c38805fbf24f020b12cefd642cf58731d
Prakhar Srivastav <prakhar1989@gmail.com>
2014-06-21 01:18:41 +0300

    Get category link

--- task3/caspertests.js
+++ task3/caspertests.js
@@ -1,9 +1,22 @@
 var casper = require('casper').create();
 
-var url = "http://m.kamajewellery.com/"
+var home_url = "http://m.kamajewellery.com/"
 
-casper.start(url, function() {
-	this.echo(this.getTitle());
+function getCategoryLink() {
+	var catLinks = document.querySelectorAll("div.full-width-card-with-img a");
+	var allLinks =  Array.prototype.map.call(catLinks, function(e) {
+		return e.getAttribute('href');
+	});
+	return allLinks[0]; // TODO: make this random
+}
+
+casper.start(home_url, function() {
+	this.echo("Begin crawling: " + this.getTitle());
+});
+
+casper.then(function() {
+	var categoryUrl = this.evaluate(getCategoryLink);
+	this.echo(categoryUrl);
 });
 
 casper.run();
