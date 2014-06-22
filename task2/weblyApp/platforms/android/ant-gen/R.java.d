bb81f25b2c8bf4a1e15c29fdeaeb8b36c2b26187
Prakhar Srivastav <prakhar1989@gmail.com>
2014-06-22 13:55:17 +0300

    Android build

--- task2/weblyApp/platforms/android/ant-gen/R.java.d
+++ task2/weblyApp/platforms/android/ant-gen/R.java.d
@@ -14,4 +14,17 @@
 /Users/asrinivasan/Code/webly/task2/weblyApp/platforms/android/res/drawable-xhdpi/icon.png \
 /Users/asrinivasan/Code/webly/task2/weblyApp/platforms/android/res/values/strings.xml \
 /Users/asrinivasan/Code/webly/task2/weblyApp/platforms/android/res/xml/config.xml \
+/Users/asrinivasan/Code/webly/task2/weblyApp/platforms/android/ant-build/res/drawable/icon.png \
+/Users/asrinivasan/Code/webly/task2/weblyApp/platforms/android/ant-build/res/drawable-hdpi/icon.png \
+/Users/asrinivasan/Code/webly/task2/weblyApp/platforms/android/ant-build/res/drawable-land-hdpi/screen.png \
+/Users/asrinivasan/Code/webly/task2/weblyApp/platforms/android/ant-build/res/drawable-land-ldpi/screen.png \
+/Users/asrinivasan/Code/webly/task2/weblyApp/platforms/android/ant-build/res/drawable-land-mdpi/screen.png \
+/Users/asrinivasan/Code/webly/task2/weblyApp/platforms/android/ant-build/res/drawable-land-xhdpi/screen.png \
+/Users/asrinivasan/Code/webly/task2/weblyApp/platforms/android/ant-build/res/drawable-ldpi/icon.png \
+/Users/asrinivasan/Code/webly/task2/weblyApp/platforms/android/ant-build/res/drawable-mdpi/icon.png \
+/Users/asrinivasan/Code/webly/task2/weblyApp/platforms/android/ant-build/res/drawable-port-hdpi/screen.png \
+/Users/asrinivasan/Code/webly/task2/weblyApp/platforms/android/ant-build/res/drawable-port-ldpi/screen.png \
+/Users/asrinivasan/Code/webly/task2/weblyApp/platforms/android/ant-build/res/drawable-port-mdpi/screen.png \
+/Users/asrinivasan/Code/webly/task2/weblyApp/platforms/android/ant-build/res/drawable-port-xhdpi/screen.png \
+/Users/asrinivasan/Code/webly/task2/weblyApp/platforms/android/ant-build/res/drawable-xhdpi/icon.png \
 /Users/asrinivasan/Code/webly/task2/weblyApp/platforms/android/ant-build/AndroidManifest.xml \
