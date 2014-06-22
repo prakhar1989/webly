8c1effa9b90baf7df981b25f365453f84c0a0136
dushyantrao <dushyant.rao@gmail.com>
2014-06-22 14:23:10 +0530

    added retailplus endpoint

--- task2/weblyApp/www/js/services.js
+++ task2/weblyApp/www/js/services.js
@@ -7,7 +7,12 @@ angular.module('starter.services', [])
   // Might use a resource here that returns a JSON array
   return{
     getEarrings: function(callback){
-      $http.get('http://localhost:3000').success(callback);
+      var config = {headers: {
+              'X-RetailPlus-Store-Id': '533ce7e652c8fdd31d8b4569 ',
+              'Accept': 'application/json;odata=verbose'
+          }
+      };
+      $http.get('http://retailplusapp.com/api/store/catalog/products/413.json',config).success(callback);
     }
   };
 
