bc26f3565e34fd33e09a54dd82191ebaae5cf7c4
Prakhar Srivastav <prakhar1989@gmail.com>
2014-06-22 20:45:48 +0300

    Added a package.json and reached pdp on webly

--- task3/caspertests.js
+++ task3/caspertests.js
@@ -1,47 +1,35 @@
-var _ = require("underscore");
 var home_url = "http://m.kamajewellery.com";
 
-function getCategoryLink() {
-  var catLinks = document.querySelectorAll("div.full-width-card-with-img a");
-  var allLinks =  Array.prototype.map.call(catLinks, function(e) {
-    return e.getAttribute('href');
-  });
-  return allLinks[0]; // TODO: make this random
-}
+casper.test.begin("Placing an order", 3, function suite(test) {
 
-// casper.test.begin(testTitle, numberOfTests, callback)
+	casper.start(home_url);
 
-casper.test.begin("Placing an order", 4, function suite(test) {
-  casper.start(home_url);
+	casper.then(function() {
+		test.assertTitle("Welcome | Kama Jewellery", "Kama Jewellery title is the one expected");
+	});
 
-  casper.then(function() {
-    test.assertTitle("Welcome | Kama Jewellery", "Kama Jewellery title is the one expected");
-  });
+	casper.then(function() {
+		// click on the first category
+		this.click("div.full-width-card-with-img a");
+	});
 
-  casper.then(function() {
-    var categoryUrl = this.evaluate(getCategoryLink);
-    test.assertEquals(home_url + categoryUrl, "http://m.kamajewellery.com/shop/category/rings/kama-platinum");
-  });
+	casper.then(function() {
+		var expectedTitle = "Platinum Rings | Kama Jewellery";
+		test.assertTitle(expectedTitle, "Category page is as expected");
+	});
 
-  casper.then(function(){
-    this.click("div.full-width-card-with-img a"); //click on platinum category
-  });
+	casper.then(function() {
+		// click on the first product
+		this.click('ul.products-grid li.product a');
+	});
 
-  casper.then(function() {
-    console.log('clicked ok, new location is ' + this.getCurrentUrl());
-    test.assertTitle("Platinum Rings | Kama Jewellery", "Platinum Jewellery title is the one expected");
-  });
+	casper.then(function() {
+		var expectedTitle = "Marisa Diamond Platinum Ring | Kama Jewellery";
+		test.assertTitle(expectedTitle, "Product page is as expected");
+	});
 
-  casper.then(function(){
-    this.click("li.item a"); //click on a ring
-  });
+	casper.run(function() {
+		test.done();
+	});
 
-  casper.then(function() {
-    console.log('clicked ok, new location is ' + this.getCurrentUrl());
-    test.assertTitle("Marisa Diamond Platinum Ring | Kama Jewellery", "Marisa Diamond title is the one expected");
-  });
-
-  casper.run(function() {
-    test.done();
-  });
 });
