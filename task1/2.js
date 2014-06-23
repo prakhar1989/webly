9b38f70f91abe33e55cab588eb97f9451997602c
Prakhar Srivastav <prakhar1989@gmail.com>
2014-06-23 17:15:43 +0300

    Fixed task 1

--- task1/2.js
+++ task1/2.js
@@ -2,13 +2,16 @@
 // fix it to log 100, 200, 300, 400, 500 etc
 
 // loops 10 times from 0 - 1000 in increments of 100
+
+// ANSWER: Create a self invoking anonymous function so that each 
+// waitFor is called with a different value of i (local to the function)
 for (var i = 0; i < 1000; i += 100) {
-  waitFor(i, function then() {
-    console.log(i)
-  })
+	(function(k) {
+		waitFor(k, function then() { console.log(k) }); // k + 100 if you need [100, 1000]
+	})(i);
 }
 
 // this will run a callback function after waiting milliseconds
 function waitFor(milliseconds, callback) {
-  setTimeout(callback, milliseconds)
+	setTimeout(callback, milliseconds)
 }
